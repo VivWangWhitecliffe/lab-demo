aa3252ade3adcc156073bbfb92fb506c32d77ed8
VivWangWhitecliffe <117702180+VivWangWhitecliffe@users.noreply.github.com>
2023-08-25 14:07:32 +1200

    change test file and cases

--- test.py
+++
@@ -1,8 +0,0 @@
-def test_incorrect():
-    assert 1+1 == 5
-
-def test_correct():
-    assert 1+1 == 2
-
-def test_incorrectwo():
-    assert 1-1 == 2
---
+++ test_maths.py
@@ -0,0 +1,10 @@
+import maths
+
+def test_addition():
+    assert maths.add(2, 3) == 5
+
+def test_subtraction():
+    assert maths.subtract(5, 3) == 2
+
+def test_wrongsubtraction():
+    assert maths.subtract(6, 4) == 2
